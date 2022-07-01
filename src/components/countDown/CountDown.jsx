4dc9723c27687d2c3c2e257f04f833425295abb8
spacegtaco <spacegtaco@gmail.com>
2022-06-30 22:22:48 -0700

    time update

--- src/components/countDown/CountDown.jsx
+++ src/components/countDown/CountDown.jsx
@@ -3,7 +3,7 @@ import CountDownTimer from '../../CountDownTimer';
 import '../../App.css';
 import './CountDown.css';
 
-const LIVE_DATE_IN_MS = new Date('July 1, 22 08:0:00 GMT+00:00');
+const LIVE_DATE_IN_MS = new Date('July 3, 22 08:0:00 GMT+00:00');
  
 const CountDown = () => {
   return (
