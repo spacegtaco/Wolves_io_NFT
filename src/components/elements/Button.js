6355ca1af619480225215441437e842d80392f14
spacegtaco <spacegtaco@gmail.com>
2022-07-16 10:01:21 -0700

    update

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+
+
 const propTypes = {
   tag: PropTypes.elementType,
   color: PropTypes.string,
